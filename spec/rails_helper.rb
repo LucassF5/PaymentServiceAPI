d8bd2e2d72799a6ce9e4eb567ac14ad748c2f24a
Lucas F <franco100lucas@gmail.com>
2025-01-07 09:02:06 -0300

    Feat: Include FactoryBot on rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,4 +67,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include FactoryBot::Syntax::Methods
+  Dir[Rails.root.join('spec/factories/**/*.rb')].each { |f| require f }
+
 end

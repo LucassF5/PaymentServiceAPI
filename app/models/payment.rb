997a2d356aaf939817c771c17826de03ffe3a7a8
Lucas F <franco100lucas@gmail.com>
2025-01-08 14:55:12 -0300

    Fix: Change the current regex

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,7 +2,7 @@ class Payment < ApplicationRecord
   validates :amount, :card_number, :name, :security_code, :valid_date, presence: true
   validates :amount, numericality: { greater_than: 0 }
   validates :card_number, length: { is: 16 }
-  validates :name, length: { in: 3..50 }, format: { with: /^[a-zA-Z\s]+$/, message: "deve conter apenas letras e espaços" }
+  validates :name, length: { in: 3..50 }, format: { with: /\A[a-zA-Z\s]+\z/, message: "deve conter apenas letras e espaços" }
   validates :security_code, length: { is: 3 }
   validates :valid_date, format: { with: /\A\d{2}\/\d{2}\z/, message: "precisa estar no formato MM/YY" }
 end
